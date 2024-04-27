f6efaaae6ffccbdc522136b7f2990c4aeb10ead3
AmanDubeyCS <147983653+AmanDubeyCS@users.noreply.github.com>
2024-04-28 01:28:39 +0530

    Deployment Issue

--- src/Components/Login/LoginForm.tsx
+++ src/Components/Login/LoginForm.tsx
@@ -78,41 +78,10 @@ export default function LoginForm() {
         </Button>
 
         <Link className="text-sm mt-3 text-right" href={"/register"}>
-          Don't have an account? <span className="underline">Register</span>
+          Don&apos;t have an account? <span className="underline">Register</span>
         </Link>
       </form>
     </div>
-    // <div className="grid place-items-center h-screen">
-    //   <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
-    //     <h1 className="text-xl font-bold my-4">Login</h1>
 
-    //     <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-    //     <Input type="email" placeholder="Email" className="focus-visible:ring-transparent" onChange={(e) => setEmail(e.target.value)}/>
-    //       {/* <input
-    //         onChange={(e) => setEmail(e.target.value)}
-    //         type="text"
-    //         placeholder="Email"
-    //       /> */}
-    //       <Input type="password" placeholder="Email"  onChange={(e) => setPassword(e.target.value)}/>
-    //       {/* <input
-    //         onChange={(e) => setPassword(e.target.value)}
-    //         type="password"
-    //         placeholder="Password"
-    //       /> */}
-    //       <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">
-    //         Login
-    //       </button>
-    //       {error && (
-    //         <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
-    //           {error}
-    //         </div>
-    //       )}
-
-    //       <Link className="text-sm mt-3 text-right" href={"/register"}>
-    //         Don't have an account? <span className="underline">Register</span>
-    //       </Link>
-    //     </form>
-    //   </div>
-    // </div>
   );
 }
